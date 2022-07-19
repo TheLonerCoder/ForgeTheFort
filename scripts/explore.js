53defe2a2b9996cf565d936db5ced637a33546a8
TheLonerCoder <thecodingloner@gmail.com>
2022-07-19 12:23:00 -0500

    layout and color functionality
    Basic implementations

---
+++ scripts/explore.js
@@ -0,0 +1,6 @@
+// TODO Check to see if a state is already selected by checking the color of the SVG (if not grey)
+
+
+function ohioClick () {
+    document.getElementById('OH').style.fill = '#432771';
+}
